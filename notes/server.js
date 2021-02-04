7fb8a56f7d0a47ef99261b96795a2d7c753a94c3
hvbairagi <hvbairagi@gmail.com>
2021-02-04 22:33:41 +0530

    hosting

--- notes/server.js
+++ notes/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/kanban"));
+app.use(express.static(__dirname + "/dist/notes"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/kanban/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/notes/index.html"));
 });
 app.listen(process.env.PORT || 8080);
